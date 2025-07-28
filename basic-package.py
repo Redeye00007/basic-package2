b9f91e69746d3b35d56f84673d3e25ae416ed442
Himal 007 <153969946+Redeye00007@users.noreply.github.com>
2025-07-29 05:36:08 +0600

    Add files via upload

---
+++ basic-package.py
@@ -0,0 +1,114 @@
+#basic package
+import os
+import sys
+import time
+
+os.system("clear")
+print("\n\n\n")
+
+ab="Wait bro System is loading...."
+
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+	
+print("\n\n\n")
+
+time.sleep(2)
+os.system("clear")
+print("\n\n\n")
+	
+ab="\033[1;32mSuccessfully Loaded Bro..\n"
+
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+
+#name= input(" \033[1;33mYour Name: ")
+ab="\033[1;32m Hey \033[1;34mWelcome to  RedEyE Family"
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)	
+	
+print("\n\n")	
+
+
+			
+						
+									
+															
+print("""\033[1;32m
+
+  _____          _ ______      ______ 
+ |  __ \\        | |  ____|    |  ____|
+ | |__) |___  __| | |__  _   _| |__   
+ |  _  // _ \\/ _` |  __|| | | |  __|  
+ | | \\ \\  __/ (_| | |___| |_| | |____ 
+ |_|  \\_\\___|\\__,_|______\__, |______|
+                          __/ |       
+                         |___/     
+                            
+\033[1;34m=================================
+\033[1;36m [✓] Owner     : Himel Majumder Pronob
+ [✓] Facebook  : Himel Majumder Pronob
+ [✓] Github    : github.com/redeye00007
+ [✓] Religion  : Bangladeshi
+ [✓] Tool Name : Basic Package install
+ [✓] Tool Status: Free
+
+ use educational propose only..
+\033[1;34m=================================\033[0m
+
+
+
+""")
+
+
+
+
+
+os.system("apt update -y")
+os.system("apt upgrade -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash -y")
+os.system ("pip install requests")
+
+
+os.system("pkg install git -y")
+os.system("pkg install ruby -y")
+os.system("pkg install wget -y")
+os.system("gem install lolcat -y")
+os.system("pkg install nmap -y")
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+
+
+print ("Allow the Button For Access the Storage in Termux")
+
+os.system ("termux-setup-storage")
+
+print("[✓] All Package installed Succecfully.....")
+
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+print("\033[95m+-------------------------------------------------+")
+slowprint('''\033[95m|             Welcome To RedEyEs World            |
+|           visit termuxcommands.com         |
+| Watch Ours Tutorials For Learn Ethical Hacking  |''')
+print("+-------------------------------------------------+")
+
+input("\n\nPress the enter key to exit : ")
